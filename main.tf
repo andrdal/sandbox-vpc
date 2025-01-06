bebf2d7c413bb1ed7de2655b3d81bf130dfbcf19
Alexandre Andrade <63659325+andrdal@users.noreply.github.com>
2025-01-06 15:19:00 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -106,7 +106,7 @@ resource "aws_route_table_association" "public" {
 
 resource "aws_nat_gateway" "main" {
   count         = var.enable_nat_gateway && var.single_nat_gateway ? 1 : 0
-  allocation_id = aws_eip.nat.id
+  allocation_id = aws_eip.nat[count.index].id # Use count.index to reference the correct instance
   subnet_id     = aws_subnet.public[0].id
 
   tags = merge(var.tags, {
@@ -114,10 +114,11 @@ resource "aws_nat_gateway" "main" {
   })
 }
 
+
 resource "aws_eip" "nat" {
   count = var.enable_nat_gateway && var.single_nat_gateway ? 1 : 0
 
-  vpc = true
+  domain = "vpc" # Replaces the deprecated 'vpc' argument
 
   tags = merge(var.tags, {
     Name = "${var.name}-nat-eip"
@@ -135,13 +136,16 @@ resource "aws_route_table" "private" {
 
 resource "aws_route" "private" {
   count = var.enable_nat_gateway && var.single_nat_gateway ? 1 : 0
-  route_table_id         = aws_route_table.private.id
+
+  route_table_id         = aws_route_table.private[count.index].id # Use count.index
   destination_cidr_block = "0.0.0.0/0"
   nat_gateway_id         = aws_nat_gateway.main[0].id
 }
 
+
 resource "aws_route_table_association" "private" {
   count          = var.enable_nat_gateway && var.single_nat_gateway ? length(var.private_subnets) : 0
+
   subnet_id      = aws_subnet.private[count.index].id
-  route_table_id = aws_route_table.private.id
+  route_table_id = aws_route_table.private[0].id # Reference the correct route table (assumes one route table for all private subnets)
 }
